05371e703978684e14d3baf224cf61af392e685b
P <pooya96@hotmail.co.uk>
2017-09-13 11:21:02 +0200

    Crud Project

---
+++ footer.php
@@ -0,0 +1,27 @@
+<style>
+#footer {
+background-color: grey;
+align-content: center;
+text-align:center;
+height: 50px;
+padding: 0.5%;
+position: fixed; 
+bottom: 0;
+left: 0;
+right: 0;
+height: 50px;
+}
+
+p { 
+color: black;
+font-family: verdana;
+font-size: 13px;
+}
+	
+</style>
+<div id="footer">
+<p>Made by <a style="text-decoration:none; color:#black;" target="new">Pooya Karimpour</a> [cph-pk126] <a style="text-decoration:none; color:#black;">CPH Business</a> Project &copy; 2016</p>
+</div>
+</body>
+</html>
+
---
+++ usingcrud.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <title>Assignment - Using CRUD</title>
+    <link rel="stylesheet" href="thecss/mystyle.css">
+    <link href='http://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet' type='text/css'>
+    </head>
+
+<body>
+
+  
+    <?php include 'menu.php';?>
+    <br>  
+   
+	<article class="article" style="margin: 3%; padding-left: 3%;">
+		<img src="er.JPG" alt="er" style="width:100%;height:100%;">
+	</article>
+
+	<?php require 'footer.php';?>
